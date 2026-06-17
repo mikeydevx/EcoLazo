8a24634d281e91a6d2ccd823d513fa3deb7b279e
ximenap <edmxim@gmail.com>
2026-06-17 14:30:19 -0600

    Update HolaMundo.java
    hola

--- src/Ejemplo/HolaMundo.java
+++ src/Ejemplo/HolaMundo.java
@@ -12,7 +12,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         
         System.out.println("Hola mundo");
-        System.out.println("chinga ");
+        System.out.println("chinga tu madre miguel ");
         
     }
 }
