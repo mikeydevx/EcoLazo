332aebc111586611bf2ce53962ef278a3138c900
hinolupita <hinolupita@gmail.com>
2026-06-19 08:26:13 -0600

    Update HolaMundo.java
    Modificacion

--- src/Ejemplo/HolaMundo.java
+++ src/Ejemplo/HolaMundo.java
@@ -12,13 +12,10 @@ public class HolaMundo {
     public static void main(String[] args) {
         
         System.out.println("Hola mundo");
-<<<<<<< HEAD
         System.out.println("Como estan?");
         
         System.out.println("Diosdado es huevon");
-=======
         System.out.println("chinga tu madre miguel ");
         
->>>>>>> 8a24634d281e91a6d2ccd823d513fa3deb7b279e
     }
 }
